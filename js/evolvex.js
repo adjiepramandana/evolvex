fe50bce012a1ab392a94adec7c7ea2e6006112a8
adjiepramandana <68036145+adjiepramandana@users.noreply.github.com>
2025-01-13 14:54:56 +0700

    Update evolvex.js

--- js/evolvex.js
+++ js/evolvex.js
@@ -1,15 +1,17 @@
-var auth = "GTXTkWCFpuAEeoyCEaLRugbqSY87CktR";
+var auth = "-Q5uMi-QHnvEVeL8745C1JXZb6EUeKWt";
+let stateLampu;
 
 function cekMode(auth) {
     $.ajax({
-        url: "https://blynk.cloud/external/api/get?token=" + auth + "&dataStreamId=" + 5,
+        url: "https://blynk.cloud/external/api/get?token=" + auth + "&dataStreamId=" + 1,
         type: "GET",
-        dataType: "json",
+        dataType: "text",
         success: function(data) {
             if (data == 1) {
                 $("#mode").html("Automated");
                 $("#toggleSwitch").prop("checked", true); // Set switch to checked
             } else {
+                updateData("v1", 0);
                 $("#mode").html("Manual");
                 $("#toggleSwitch").prop("checked", false); // Set switch to unchecked
             }
@@ -48,31 +50,61 @@ function updateData(pin, value) {
         success: function(data) {
             if (data == 1) {
                 // Beri waktu untuk memastikan perubahan diterapkan di server
-                setTimeout(() => cekMode(auth), 500); // Delay 500ms
+                
             } else {
                 alert("Failed to update data.");
             }
         },
-        error: function() {
-            console.log(data);
-            alert("Error occurred during update.");
+        error: function(data) {
+            
         }
     });
 }
+
+function cekLampu(id){
+    $.ajax({
+        url: "https://blynk.cloud/external/api/get?token=" + auth + "&dataStreamId=" + id,
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            if (data == 1) {
+                stateLampu = 1;
+                $("#eris").attr("src", "https://hosting.tigerengine.id/mv3j9o.png");
+            } else {
+                stateLampu = 0;
+                $("#eris").attr("src", "https://hosting.tigerengine.id/l5o9ty.png");
+            }
+        },
+        error: function() {
+            alert("Failed to fetch lampu data.");
+        }
+    });
+}
+
+function toggleLampu(pin){
+    if (stateLampu == 1) {
+        updateData("v1", 0);
+    } else {
+        updateData("v1", 1);
+    }
+}
+
 
 $(document).ready(function() {
     $("#djload").show(); // Show loading icon
     setTimeout(function() {
         cekKonek(auth);      // Check connection status
-        cekMode(auth);          // Check mode 
+        cekMode(auth);
+        cekLampu(2);          // Check mode 
         $("#djload").hide(); // Hide loading icon
     }, 1000);
 
     // Set interval to keep checking connection and mode
     setInterval(() => {
-        cekKonek(auth);
         cekMode(auth);
-    }, 200); // Check every 5 seconds
+        cekLampu(2);
+    }, 500); // Check every 5 seconds
+
 });
 
 function refresh() {
